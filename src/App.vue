fab147c3a79093cc6938c1a6251720139d46d8dc
Emanuel Cabrera <emanuelc@versiondos.com>
2023-04-16 18:28:30 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
+  <Navbar />
   <img alt="Vue logo" src="./assets/logo.png">
+  <router-view></router-view>
 </template>
 
 <script>
+  // import Navbar from './modules/shared/components/Navbar.vue';
+import { defineAsyncComponent } from 'vue';
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Navbar: defineAsyncComponent(() => import(/* webpackChunkName:"Navbar" */ '@/modules/shared/components/Navbar.vue'))
+  }
 }
 </script>
 
---
+++ src/modules/shared/pages/NoPageFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>No Page Found</h1>
+    <h1>404</h1>
+</template>
