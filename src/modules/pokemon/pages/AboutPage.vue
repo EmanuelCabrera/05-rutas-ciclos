553bd2ac18de51675ad59c1a0e62cd06652fedd6
Emanuel Cabrera <emanuelc@versiondos.com>
2023-04-21 21:27:43 -0300

    props

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,68 @@
 <template>
     <h1>About Page</h1>
+    <hr>
+    <h2>{{  name }}</h2>
+    <button @click="onChangeName">
+        Cambiar nombre
+    </button>
 </template>
+
+<script>
+    export default{
+
+
+        data() {
+            return {
+                name: 'Emanuel'
+            }
+        },
+
+        methods: {
+            onChangeName (){
+                console.log("se cambiara el nombre a Lautaro");
+                this.name = 'Lautaro'
+            }
+        }, 
+        beforeCreate() {
+            console.log('beforeCreate');
+        },
+        created() {
+            console.log('created');
+            //PETICIONES HTTP
+        },
+        beforeMount() {
+            console.log('beforeMount');
+        },
+        mounted() {
+            console.log('mounted');
+        },
+        beforeUpdate() {
+            console.log('beforeUpdate');
+        },
+        updated() {
+            console.log('updated');
+        },
+        activated() {
+            console.log('activated');
+        },
+        deactivated() {
+            console.log('deactivated');
+        },
+        beforeUnmount() {
+            console.log('beforeUnmount');
+        },
+        unmounted() {
+            console.log('unmounted');
+        },
+        errorCaptured() {
+            console.log('errorCaptured');
+        },
+        renderTracked() {
+            console.log('renderTracked');
+        },
+        renderTriggered() {
+            console.log('renderTriggered');
+        },
+    }
+
+</script>
--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Pokemon Page</h1>
+    <h1>Pokemon: <span>#{{ id }}</span></h1>
 </template>
+
+<script>
+    export default{
+
+        props: {
+            id: {
+                type: Number,
+                required: true
+            }
+        },
+        data() {
+            return {
+                // id: null
+            }
+        },
+        created(){
+            // console.log( this.$route );
+            const { id } = this.$route.params
+            console.log("🚀 ~ file: PokemonPage.vue:10 ~ created ~ id:", id)
+            // this.id = id
+        }
+    }
+</script>
